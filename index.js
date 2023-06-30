9ed5771200af52b32c9bdf05050a0d2afdbf529b
Grant Duntugan <GDuntugan@mednet.ucla.edu>
2023-06-30 15:18:33 -0700

    Added question title, reworked some toggle natures, made it so you can click on anywhere on question box instead of just number

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 let body = document.getElementsByTagName('body')[0];
-let transitionState = 0;
+let transitionState = "title";
 body.addEventListener('click', () => {
-    if (transitionState == 0) {
+    if (transitionState === "title") {
         let sect = document.getElementsByTagName("section")[0];
-        sect.classList.toggle("fade-out");
+        sect.classList.add("fade-out");
         setTimeout(() => {
-            sect.classList.toggle("d-none");
+            sect.classList.add("d-none");
             body.style.backgroundColor = "#213061";
-            body.classList.toggle("bg-primary");
+            body.classList.remove("bg-primary");
             sect = document.getElementsByTagName("section")[1];
-            sect.classList.toggle("d-none");
-            sect.classList.toggle("fade-in");
+            sect.classList.remove("d-none");
+            sect.classList.add("fade-in");
+            document.querySelector(".question").classList.remove("d-none");
 
         }, 1500);
-        transitionState++;
+        transitionState = "displaying question";
     }
+
+    else if (transitionState == "displaying question") {
+        console.log("dflsk")
+        let question = document.querySelector(".question")
+        question.classList.toggle("fade-out");
+        setTimeout(() => {
+            question.classList.toggle("d-none");
+            let board = document.querySelector(".board");
+            board.classList.toggle("d-none");
+            board.classList.toggle("fade-in");
+        }, 1500);
+        transitionState = "displaying board";
+    }
+
+    //TODO: switch back to displaying question under some circumstance like arrow button
+
 });
 
+let answerBoxes = document.querySelectorAll(".answer-box");
 let concealed = document.querySelectorAll(".concealed");
 let answers = document.querySelectorAll(".answer");
-for (let i = 0; i < concealed.length; i++) {
-    concealed[i].addEventListener('click', () => {
-        concealed[i].classList.toggle("d-none");
-        answers[i].classList.toggle("d-none");
-        answers[i].classList.toggle("fade-in");
+for (let i = 0; i < answerBoxes.length; i++) {
+    answerBoxes[i].addEventListener('click', () => {
+        concealed[i].classList.add("d-none");
+        answers[i].classList.remove("d-none");
+        answers[i].classList.add("fade-in");
     });
 }
